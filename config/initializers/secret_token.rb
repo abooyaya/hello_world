14b537a430e78f362817e667ed23ad43e066e30c
Ben <ben@BentekiMacBook-Air.local>
2014-04-01 16:13:20 +0800

    add init rails  app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '22b654d2dadcc9a9cf80a36f0e23c635f0cf5b4931c7313097706e934d9d50f49cd9d0bac22567086306604c379917c963f3486086b231868107b2f2e413e932'
